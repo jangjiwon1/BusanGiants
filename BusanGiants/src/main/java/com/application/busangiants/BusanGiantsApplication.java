6cf7a2ca964834f4c4b118268424c60ddd5329db
19_web_jjw <19_web_jjw@DESKTOP-HOGPRH8>
2024-07-05 21:42:20 +0900

    2024-07-05 학원에서 커밋

---
+++ BusanGiants/src/main/java/com/application/busangiants/BusanGiantsApplication.java
@@ -0,0 +1,13 @@
+package com.application.busangiants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusanGiantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusanGiantsApplication.class, args);
+	}
+
+}
