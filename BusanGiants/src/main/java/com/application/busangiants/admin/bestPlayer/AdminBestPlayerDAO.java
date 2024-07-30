f0627492813ffbaa92a7f1dd9b09ad993098578c
19_web_jjw <19_web_jjw@DESKTOP-HOGPRH8>
2024-07-30 21:50:23 +0900

    Bestplayer Mapper 만들고 html 마무리

--- BusanGiants/src/main/java/com/application/busangiants/admin/bestPlayer/AdminBestPlayerDAO.java
+++ BusanGiants/src/main/java/com/application/busangiants/admin/bestPlayer/AdminBestPlayerDAO.java
@@ -2,8 +2,11 @@ package com.application.busangiants.admin.bestPlayer;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.application.busangiants.bestPlayer.BestPlayerDTO;
 
+@Mapper
 public interface AdminBestPlayerDAO {
 	
 	public List<BestPlayerDTO>getBestPlayerList();
